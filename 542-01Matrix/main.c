41a6dd0b04b4d425adf98733ff54bac93b64a427
geniusrabbit <U201613566@hust.edu.cn>
2017-08-27 20:47:51 +0800

    8-27

--- 542-01Matrix/main.c
+++ 542-01Matrix/main.c
@@ -25,8 +25,9 @@ bool Queue(){
 
 }
  */
+int** updateMatrix(int** matrix, int matrixRowSize, int matrixColSize, int** columnSizes, int* returnSize);
+
 int main() {
-    printf("Hello, World!\n");
     return 0;
 }
 /**
@@ -35,73 +36,85 @@ int main() {
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 int** updateMatrix(int** matrix, int matrixRowSize, int matrixColSize, int** columnSizes, int* returnSize) {
-    //首先复制整个数组并将所有的o入队
-    //等入队完毕之后开始出对，终结条件为队列为空
-    //出队时，将该元素周围所有的1入队并更改其值
-    //队为空数组完成
+
     int i,j;
     int front=0,rear=0;
     *returnSize=matrixRowSize;
 
-    int **return_matrix=(int**)malloc(matrixRowSize* sizeof(int*));
-    (*columnSizes)=(int*)malloc(sizeof(int));
 
-    for (i = 0; i <matrixRowSize; ++i) {
-        return_matrix[i]=(int*)malloc(matrixColSize* sizeof(int));
-        (*columnSizes)[i]=matrixColSize;
-    }
+    int t=matrixRowSize*matrixColSize;
 
-    int **Queue=(int**)malloc(10000*sizeof(int*));
+    int **Queue=(int**)malloc(t*sizeof(int*));
 
-    for (i = 0; i <10000; ++i) {
+    for (i = 0; i <t; i++) {
         Queue[i]=(int*)malloc(3* sizeof(int));
     }
-    //将所有的0入队
+
+    int **return_matrix=(int**)malloc(matrixRowSize* sizeof(int*));
+    (*columnSizes)=(int*)malloc(matrixRowSize*sizeof(int));
+
+    //首先复制整个数组并将所有的o入队
+    //等入队完毕之后开始出对，终结条件为队列为空
+    //出队时，将该元素周围所有的1入队并更改其值
+    //队为空数组完成
+
     for(i=0;i<matrixRowSize;i++){
+        return_matrix[i]=(int*)malloc(matrixColSize* sizeof(int));
+        (*columnSizes)[i]=matrixColSize;
         for(j=0;j<matrixColSize;j++){
             return_matrix[i][j]=matrix[i][j];
             if(matrix[i][j]==0){
+                matrix[i][j]=-1;
                 Queue[front][0]=i;
                 Queue[front][1]=j;
-                Queue[front][0]=matrix[i][j];
+                Queue[front][2]=0;
                 front++;
             }
         }
     }
-    //开始出队，同时入队
+
     while(front!=rear){
+        //开始出队，同时入队
         //满足条件则入队
         //找出出队的那个上下左右是否满足入队的条件，若没有超过边界，值等于1则入队
         //up
-        if(Queue[rear][0]-1>0&&matrix[Queue[rear][0]-1][Queue[rear][1]]==1){
+        if(Queue[rear][0]-1>=0&&return_matrix[Queue[rear][0]-1][Queue[rear][1]]==1&&matrix[Queue[rear][0]-1][Queue[rear][1]]!=-1){
             Queue[front][0]=Queue[rear][0]-1;
             Queue[front][1]=Queue[rear][1];
-            Queue[front][3]=Queue[rear][3]+1;
-            return_matrix[Queue[front][0]][Queue[front][1]]=Queue[front][3];
+
+            Queue[front][2]=Queue[rear][2]+1;
+            return_matrix[Queue[front][0]][Queue[front][1]]=Queue[front][2];
+            matrix[Queue[front][0]][Queue[front][1]]=-1;
             front++;
         }
         //down
-        if(Queue[rear][0]+1<matrixRowSize&&matrix[Queue[rear][0]+1][Queue[rear][1]]==1){
+        if(Queue[rear][0]+1<matrixRowSize&&return_matrix[Queue[rear][0]+1][Queue[rear][1]]==1&&matrix[Queue[rear][0]+1][Queue[rear][1]]!=-1){
             Queue[front][0]=Queue[rear][0]+1;
             Queue[front][1]=Queue[rear][1];
-            Queue[front][3]=Queue[rear][3]+1;
-            return_matrix[Queue[front][0]][Queue[front][1]]=Queue[front][3];
+
+            Queue[front][2]=Queue[rear][2]+1;
+            return_matrix[Queue[front][0]][Queue[front][1]]=Queue[front][2];
+            matrix[Queue[front][0]][Queue[front][1]]=-1;
             front++;
         }
         //right
-        if(Queue[rear][1]+1<matrixColSize&&matrix[Queue[rear][0]+1][Queue[rear][1]]==1){
+        if(Queue[rear][1]+1<matrixColSize&&return_matrix[Queue[rear][0]][Queue[rear][1]+1]==1&&matrix[Queue[rear][0]][Queue[rear][1]+1]!=-1){
             Queue[front][0]=Queue[rear][0];
             Queue[front][1]=Queue[rear][1]+1;
-            Queue[front][3]=Queue[rear][3]+1;
-            return_matrix[Queue[front][0]][Queue[front][1]]=Queue[front][3];
+
+            Queue[front][2]=Queue[rear][2]+1;
+            return_matrix[Queue[front][0]][Queue[front][1]]=Queue[front][2];
+            matrix[Queue[front][0]][Queue[front][1]]=-1;
             front++;
         }
         //left
-        if(Queue[rear][1]-1>0&&matrix[Queue[rear][0]-1][Queue[rear][1]]==1){
+        if(Queue[rear][1]-1>=0&&return_matrix[Queue[rear][0]][Queue[rear][1]-1]==1&&matrix[Queue[rear][0]][Queue[rear][1]-1]!=-1){
             Queue[front][0]=Queue[rear][0];
             Queue[front][1]=Queue[rear][1]-1;
-            Queue[front][3]=Queue[rear][3]+1;
-            return_matrix[Queue[front][0]][Queue[front][1]]=Queue[front][3];
+
+            Queue[front][2]=Queue[rear][2]+1;
+            return_matrix[Queue[front][0]][Queue[front][1]]=Queue[front][2];
+            matrix[Queue[front][0]][Queue[front][1]]=-1;
             front++;
         }
 
@@ -109,6 +122,4 @@ int** updateMatrix(int** matrix, int matrixRowSize, int matrixColSize, int** col
     }
 
     return return_matrix;
-
-
 }
