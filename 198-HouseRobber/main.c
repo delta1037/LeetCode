80777c427943cf14e33c20f5af23d0340ea6ab80
geniusrabbit <U201613566@hust.edu.cn>
2017-09-03 19:00:54 +0800

    9-3

--- 198-HouseRobber/main.c
+++ 198-HouseRobber/main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 
 int rob(int* nums, int numsSize) {
-    
+    int i;
+
+    int t=0;
+    int m;
+    if(numsSize<=1){
+        return numsSize==0? 0:nums[1];
+    }
+
+    int dp[numsSize];
+
+    dp[0]=nums[0];
+    dp[1]=nums[1];
+    t=dp[0]>dp[1]? dp[0]:dp[1];
+
+    m=dp[0];
+
+    for(i=2;i<numsSize;i++){
+        dp[i]=(dp[i-1]>=(nums[i]+m)? dp[i-1]:m+nums[i]);
+        m=m>dp[i-1]? m:dp[i-1];
+        t=dp[i];
+    }
+
+    return t;
 }
 int main() {
-    printf("Hello, World!\n");
+    int a[4]={2,1,1,2};
+    int re;
+    re=rob(a,4);
+    printf("%d",re);
     return 0;
 }
---
+++ 300-LongestIncreasingSubsequence/cmake-build-debug/CMakeFiles/300_LongestIncreasingSubsequence.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/300_LongestIncreasingSubsequence.dir/main.c.o"
+  "300_LongestIncreasingSubsequence.pdb"
+  "300_LongestIncreasingSubsequence"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/300_LongestIncreasingSubsequence.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
