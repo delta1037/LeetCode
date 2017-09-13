ba24e78542fa83bc9fd249e1d7f5e081115a540a
geniusrabbit <U201613566@hust.edu.cn>
2017-09-13 14:09:31 +0800

    9-13

--- 673-NumberofLongestIncreasingSubsequence/main.c
+++ 673-NumberofLongestIncreasingSubsequence/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
 int findNumberOfLIS(int* nums, int numsSize) {
+    if(numsSize==0){
+        return 0;
+    }
     int i,j;
-    for(i=0;i<numsSize;i++){
+    int dp[numsSize];
+    dp[0]=1;
+    int result=1,max=1;
+    int max_num=nums[0];
+    int min_num=nums[0];
+
+    for(i=1;i<numsSize;i++){
+        if(nums[i]>max_num){
+            max_num=nums[i];
+            result++;
+        }
 
     }
+
+    return result;
 }
 int main() {
     int num[5]={1,3,5,4,7};
