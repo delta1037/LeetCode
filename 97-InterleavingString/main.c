29096fd32ccc7baedbb0755a2a1c7bfbbf0ff7e2
geniusrabbit <U201613566@hust.edu.cn>
2017-09-27 12:47:15 +0800

    9-27

--- 97-InterleavingString/main.c
+++ 97-InterleavingString/main.c
@@ -4,26 +4,29 @@
 #include <malloc.h>
 
 bool isInterleave(char* s1, char* s2, char* s3) {
-
     int nums3=(int)strlen(s3);
     char* ss=(char*)malloc(nums3*sizeof(char));
     int i=0;
     int j=0;
     int k=0;
-    while (s3[j]!='\0'&&s2[i]!='\0'){
-        if(s3[j]==s2[i]){
+    while (s3[j]!='\0'){
+        if(s3[j]==s2[i]&&s2[i]!='\0'){
             j++;
             i++;
         }
         else{
             ss[k]=s3[j];
             ss[k+1]='\0';
+            k++;
             j++;
         }
     }
+
     if(s2[i]!='\0'){
         return false;
     }
+
+    i=0;
 
     while (ss[i]!='\0'){
         if(ss[i]==s1[i]){
@@ -33,9 +36,11 @@ bool isInterleave(char* s1, char* s2, char* s3) {
             return false;
         }
     }
+    if(s1[i]!='\0'){
+        return false;
+    }
 
     return true;
-
 }
 int main() {
     printf("Hello, World!\n");
