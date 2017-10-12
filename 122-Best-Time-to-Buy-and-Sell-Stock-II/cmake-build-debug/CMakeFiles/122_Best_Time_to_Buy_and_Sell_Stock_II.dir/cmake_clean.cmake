96dd2e6020552d86960f3300e3ef567136cd4fa2
geniusrabbit <U201613566@hust.edu.cn>
2017-10-12 13:32:40 +0800

    10-12

---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/cmake-build-debug/CMakeFiles/122_Best_Time_to_Buy_and_Sell_Stock_II.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/122_Best_Time_to_Buy_and_Sell_Stock_II.dir/main.c.o"
+  "122_Best_Time_to_Buy_and_Sell_Stock_II.pdb"
+  "122_Best_Time_to_Buy_and_Sell_Stock_II"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/122_Best_Time_to_Buy_and_Sell_Stock_II.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int maxProfit(int* prices, int pricesSize) {
+    int max=0;
+    int i;
+    int t;
+
+    for(i=1;i<pricesSize;i++){
+        t=prices[i]-prices[i-1];
+        max=max+(t>0?  t:0);
+    }
+
+    return max;
+}
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
