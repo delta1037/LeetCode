e072a4563ae816ce9966e10b36b302c59be532a5
geniusrabbit <U201613566@hust.edu.cn>
2017-10-17 21:25:09 +0800

    10-17

---
+++ 237-DeleteNode-in-a-LinkedList/cmake-build-debug/CMakeFiles/237_DeleteNode_in_a_LinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/237_DeleteNode_in_a_LinkedList.dir/main.c.o"
+  "237_DeleteNode_in_a_LinkedList.pdb"
+  "237_DeleteNode_in_a_LinkedList"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/237_DeleteNode_in_a_LinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
