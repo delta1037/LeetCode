789c4c3d0c7708c6a6acc3b2e5b93531d05f8b90
genius-rabbit <U201613566@hust.edu.cn>
2018-07-01 17:25:09 +0800

    2018-07-01

---
+++ 77-Combinations/cmake-build-debug/CMakeFiles/77_Combinations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/77_Combinations.dir/main.cpp.o"
+  "77_Combinations.pdb"
+  "77_Combinations"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/77_Combinations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
